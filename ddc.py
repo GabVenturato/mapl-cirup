87191e8e34709626c562ee4b36f994ea38ce8020
Gabriele Venturato <venturato.gabriele@gmail.com>
2022-11-22 13:26:53 +0100

    Prune circuit of double AND nodes

--- ddc.py
+++ ddc.py
@@ -15,16 +15,67 @@ Label = namedtuple('Label', 'prob, util, dec')
 VarIndex = namedtuple('VarIndex', 'pos, neg')
 
 
+class MaxEUSemiring:
+    _empty_set = set()
+
+    def __init__(self, decisions):
+        self.val_zero = Label(0.0, 0.0, decisions)
+        self.zero_decision_length = len(decisions)
+
+    def one(self):
+        return Label(1.0, 0.0, self._empty_set)
+
+    def zero(self):
+        return self.val_zero
+
+    def plus(self, a: Label, b: Label):
+        p_a, eu_a, d_a = a
+        p_b, eu_b, d_b = b
+        if len(d_a) or len(d_b):
+            # Max
+            if len(d_b) == self.zero_decision_length:  # avoid false
+                result = a
+            elif len(d_a) == self.zero_decision_length:  # avoid false
+                result = b
+            elif p_a == 0:
+                result = b
+            elif p_b == 0:
+                result = a
+            elif eu_a / p_a >= eu_b / p_b:  # check on eu
+                result = a
+            else:
+                result = b
+            print("max( (%s, %s, %s), (%s, %s, %s) ) = (%s, %s, %s)" %
+                  (a.prob, a.util, a.dec, b.prob, b.util, b.dec, result.prob, result.util, result.dec))
+            return result
+        else:
+            # Sum
+            print("(%s, %s, %s) + (%s, %s, %s) ) = (%s, %s, %s)" %
+                  (a.prob, a.util, a.dec, b.prob, b.util, b.dec, p_a + p_b, eu_a + eu_b, self._empty_set))
+            return Label(p_a + p_b, eu_a + eu_b, self._empty_set)
+
+    def times(self, a: Label, b: Label):
+        p_a, eu_a, d_a = a
+        p_b, eu_b, d_b = b
+        eu_n = p_a * eu_b + p_b * eu_a
+        print("(%s, %s, %s) * (%s, %s, %s) ) = (%s, %s, %s)" %
+              (a.prob, a.util, a.dec, b.prob, b.util, b.dec, p_a * p_b, eu_n, d_a.union(d_b)))
+        return Label(p_a * p_b, eu_n, d_a.union(d_b))
+
+
 class DDC:
     """
     Class for dynamic decision circuits (DDCs).
     """
     _false = -1
+    _decisions: Set[int] = set()
+    _semiring = None
+    _id = 1
 
     def __init__(self):
         self._root: int = self._false
-        self._children: List[List[int]] = [[]]
-        self._type: List[NodeType] = [NodeType.TRUE]
+        self._children: Dict[int, List[int]] = {0: []}
+        self._type: Dict[int, NodeType] = {0: NodeType.TRUE}
         self._var2node: Dict[str, VarIndex] = {'true': VarIndex(0, 0)}
         self._label: Dict[int, Label] = dict()
 
@@ -57,14 +108,19 @@ class DDC:
         for key in weights:
             index = sdd.atom2var.get(abs(key), -1)
             if isinstance(weights[key], bool) and weights[key] is True:
-                ddc._set_pn_labels(literal_id2name[index][0], key > 0, Label(1, 0, set()))
+                if key > 0:
+                    ddc._set_positive_label(literal_id2name[index][0], Label(1.0, 0.0, set()))
+                else:
+                    ddc._set_negative_label(literal_id2name[index][0], Label(1.0, 0.0, set()))
             elif isinstance(weights[key], Constant):  # probability (utilities at the beginning are all zero)
                 prob: float = weights[key].compute_value()
-                ddc._set_pn_labels(literal_id2name[index][0], key > 0, Label(prob, 0, set()))
+                ddc._set_pn_labels(literal_id2name[index][0], key > 0, Label(prob, 0.0, set()))
             elif weights[key] == Term("?"):  # decision
                 node_id = ddc._var2node[literal_id2name[index][0]]
-                ddc._set_positive_label(literal_id2name[index][0], Label(1, 0, {node_id.pos}))
-                ddc._set_negative_label(literal_id2name[index][0], Label(1, 0, {node_id.neg}))
+                ddc._decisions.add(node_id.pos)
+                ddc._decisions.add(node_id.neg)
+                ddc._set_positive_label(literal_id2name[index][0], Label(1.0, 0.0, {node_id.pos}))
+                ddc._set_negative_label(literal_id2name[index][0], Label(1.0, 0.0, {node_id.neg}))
 
         # for reward parameters
         for r in rewards:
@@ -83,9 +139,9 @@ class DDC:
                 ddc._update_util_negative_label(str(r), rewards[r].compute_value())
 
         # for all the other literals
-        for node_id, node_type in enumerate(ddc._type):
+        for node_id, node_type in ddc._type.items():
             if node_type == NodeType.LITERAL and node_id not in ddc._label:
-                ddc._label[node_id] = Label(1, 0, set())
+                ddc._label[node_id] = Label(1.0, 0.0, set())
 
         return ddc
 
@@ -114,9 +170,10 @@ class DDC:
                         return self._var2node[var_name].neg
 
             # Otherwise, insert it in the circuit
-            self._children.append([])
-            self._type.append(NodeType.LITERAL)
-            node_id: int = len(self._children) - 1
+            node_id = self._id
+            self._id += 1
+            self._children[node_id] = []
+            self._type[node_id] = NodeType.LITERAL
             visited[node.id] = node_id
 
             # update the var2node mapping
@@ -148,18 +205,32 @@ class DDC:
                     prime_node = self._compact_sdd(prime, lit_name_map, visited)
                     assert prime_node != self._false, "Vincent was wrong: prime can be false."
                     # Create AND node
-                    self._children.append([sub_node, prime_node])
-                    self._type.append(NodeType.AND)
-                    or_children.append(len(self._children) - 1)
+                    # TODO Check if it's ok to remove AND nodes (tricky if an AND node is child of both an AND and OR)
+                    sub_children = [sub_node]
+                    if self._type[sub_node] == NodeType.AND:
+                        sub_children = self._children[sub_node]
+                        self._children.pop(sub_node)
+                        self._type.pop(sub_node)
+                    prime_children = [prime_node]
+                    if self._type[prime_node] == NodeType.AND:
+                        prime_children = self._children[prime_node]
+                        self._children.pop(prime_node)
+                        self._type.pop(prime_node)
+                    node_id = self._id
+                    self._id += 1
+                    self._children[node_id] = sub_children + prime_children
+                    self._type[node_id] = NodeType.AND
+                    or_children.append(node_id)
             # Create OR node
             if len(or_children) == 0:
                 return self._false
             elif len(or_children) == 1:
                 return or_children[0]
             else:
-                self._children.append(or_children)
-                self._type.append(NodeType.OR)
-                node_id = len(self._children) - 1
+                node_id = self._id
+                self._id += 1
+                self._children[node_id] = or_children
+                self._type[node_id] = NodeType.OR
                 visited[node.id] = node_id
                 return node_id
 
@@ -180,7 +251,7 @@ class DDC:
             "digraph sdd {",
             "overlap=false;"
         ]
-        for node, children in enumerate(self._children):
+        for node, children in self._children.items():
             dot_node = ""
             if self._type[node] == NodeType.TRUE:
                 dot_node = f"{node} [shape=rectangle,label=\"True\"];"
@@ -191,7 +262,7 @@ class DDC:
                 if len(dec) > 0:
                     decs = self._node_to_var(dec.pop())
                     dec_label = "{" + ", ".join(decs) + "}"
-                label = f"({self._label[node].prob}, {self._label[node].util}, {dec_label})"
+                label = f"({round(self._label[node].prob, 2)}, {self._label[node].util}, {dec_label})"
                 dot_node = f"{node} [shape=rectangle,label=\"{var_name} : {label}\"];"
             elif self._type[node] == NodeType.AND or self._type[node] == NodeType.OR:
                 var_name = '+' if self._type[node] == NodeType.OR else '×'
@@ -228,7 +299,7 @@ class DDC:
             self._set_negative_label(var, Label(1-label.prob, label.util, label.dec))
         else:
             self._set_negative_label(var, label)
-            self._set_positive_label(var, Label(1 - label.prob, label.util, label.dec))
+            self._set_positive_label(var, Label(1-label.prob, label.util, label.dec))
 
     def _update_util_positive_label(self, var: str, util: float):
         index = self._var2node[var]
@@ -243,13 +314,39 @@ class DDC:
     def size(self) -> int:
         return len(self._children)
 
+    def maxeu(self) -> Label:
+        return self.evaluate(MaxEUSemiring(self._decisions))
+
+    def evaluate(self, semiring: MaxEUSemiring) -> Label:
+        self._semiring = semiring
+        return self._evaluate_node(self._root)
+
+    def _evaluate_node(self, node: int) -> Label:
+        if self._type[node] == NodeType.TRUE:
+            return self._semiring.one()
+        elif self._type[node] == NodeType.LITERAL:
+            return self._label[node]
+        elif self._type[node] == NodeType.OR:
+            total = self._semiring.zero()
+            for child in self._children[node]:
+                child_eval = self._evaluate_node(child)
+                new_total = self._semiring.plus(total, child_eval)
+                total = new_total
+            return total
+        elif self._type[node] == NodeType.AND:
+            total = self._semiring.one()
+            for child in self._children[node]:
+                child_eval = self._evaluate_node(child)
+                new_total = self._semiring.times(total, child_eval)
+                total = new_total
+            return total
+
 
 class NodeType(Enum):
     TRUE = 1
     LITERAL = 2
     AND = 3
     OR = 4
-
 
 # class Label:
 #     def __init__(self, prob: float, util: float, dec: Set[str]):  # TODO float must be changed to some Numpy format
