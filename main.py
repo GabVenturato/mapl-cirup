341300d3ceeea8aa43da109555c9feb51239f772
Gabriele Venturato <venturato.gabriele@gmail.com>
2023-02-03 09:59:39 +0100

    Prepare main for experimental evaluation

--- main.py
+++ main.py
@@ -3,44 +3,49 @@ import sys
 from problog.util import init_logger
 from mapl_cirup import MaplCirup
 
-MAX_ACTS = 10   # Max number of steps in the online algorithm
-
 
 def main(argv):
     args = argparser().parse_args(argv)
 
-    input_file = args.inputfile
-    init_logger(args.verbose)
+    if args.experiments:
+        print("I'll run the experimental evaluation")
+    else:
+        if args.file is None:
+            input_file = input("File path: ")
+        else:
+            input_file = args.file
+        init_logger(args.verbose)
 
-    mc: MaplCirup = MaplCirup(input_file)
+        mc: MaplCirup = MaplCirup(input_file)
 
-    print("Compile time: %s" % mc.compile_time())
-    # print("Minimize time: %s" % mc.minimize_time())
-    print("Circuit size: %s" % mc.size())
+        print("Compile time: %s" % mc.compile_time())
+        # print("Minimize time: %s" % mc.minimize_time())
+        print("Circuit size: %s" % mc.size())
 
-    # mc.value_iteration(discount=0.9, error=0.01)
-    mc.value_iteration(discount=0.9, error=0.1)
-    # mc.value_iteration(discount=0.9)
-    # mc.value_iteration(discount=0.9, horizon=1)
-    # mc.value_iteration(horizon=10)
-    # mc.value_iteration()  # = immediate reward
+        # mc.value_iteration(discount=0.9, error=0.01)
+        mc.value_iteration(discount=0.9, error=0.1)
+        # mc.value_iteration(discount=0.9)
+        # mc.value_iteration(discount=0.9, horizon=1)
+        # mc.value_iteration(horizon=10)
+        # mc.value_iteration()  # = immediate reward
 
-    print("Value iteration time: %s" % mc.value_iteration_time())
-    print("Total time: %s" % mc.tot_time())
-    print('\nNumber of iterations: ' + str(mc.iterations()) + '\n')
-    # mc.view_dot()
-    # mc.print_explicit_policy()
+        print("Value iteration time: %s" % mc.value_iteration_time())
+        print("Total time: %s" % mc.tot_time())
+        print('\nNumber of iterations: ' + str(mc.iterations()) + '\n')
+        # mc.view_dot()
+        # mc.print_explicit_policy()
 
 
 def argparser():
     import argparse
 
-    parser = argparse.ArgumentParser(description="Dynamic Decision Circuits solver based on ProbLog.")
-    parser.add_argument('inputfile')
+    parser = argparse.ArgumentParser(description="MArkov PLanning with CIRcuit bellman UPdates.")
+    parser.add_argument('-f', '--file', help='Path to the input file')
     # TODO: Implement the following parameter
     # parser.add_argument('-o', '--output', type=str, default=None,
     #                     help='Write output to given file (default: stdout)')
     parser.add_argument('-v', '--verbose', help='Verbose mode')
+    parser.add_argument('--experiments', help='Run experimental evaluation', action='store_true')
     return parser
 
 
