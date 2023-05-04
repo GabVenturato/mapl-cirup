3593fa696ccc66b418f7dc1c0a6dfb716acbcefb
Gabriele Venturato <venturato.gabriele@gmail.com>
2023-05-04 17:21:40 +0200

    New experiments for cross stitch

--- examples/synth_struct_cross_stitch/synth_struct_cross_stitch_gen.py
+++ examples/synth_struct_cross_stitch/synth_struct_cross_stitch_gen.py
@@ -261,7 +261,7 @@ def gen_spudd_model(dec_num, var_num, reward):
 
 if __name__ == '__main__':
     for dec_num in range(1, 2):
-        for var_num in range(2, 11):
+        for var_num in range(11, 17):
             trans = gen_transition(dec_num, var_num)
             print("\nTransition:")
             for el in trans:
@@ -278,4 +278,4 @@ if __name__ == '__main__':
                 print(str(el) + " => " + str(rew[el]))
 
             gen_mc_model(dec_num, var_num, trans, struct, rew)
-            gen_spudd_model(dec_num, var_num, rew)
+            # gen_spudd_model(dec_num, var_num, rew)
--- plots/synth_struct_combined_plot_vs_spudd.py
+++ plots/synth_struct_combined_plot_vs_spudd.py
@@ -13,7 +13,7 @@ def _get_synth_chain_data():
     df = df.loc[df['family'] == 'synth_struct_chain']
 
     df = df[df['var_num'] > 1]  # skip first because times are zero (not well pictured in log scale)
-    df = df[df['var_num'] <= 8]  # skip >= 8 because all timeouts
+    # df = df[df['var_num'] <= 8]  # skip >= 8 because all timeouts
     # max_vars = df['var_num'].max()
     # max_vars = 8
 
@@ -27,16 +27,24 @@ def _get_synth_chain_data():
 
     df_compilation = df.loc[df['solver'] == 'mapl-cirup']
     df_compilation = df_compilation[['run', 'solver', 'var_num', 'tot_time']]
-    df_compilation = df_compilation.replace('mapl-cirup', 'maple-cirup (kc+vi)')
+    df_compilation = df_compilation.replace('mapl-cirup', 'mapl-cirup (kc+vi)')
     df_compilation = df_compilation.groupby(['solver', 'var_num'], as_index=False).agg({'tot_time': ['mean', 'std']})
     df_compilation.columns = ['solver', 'var_num', 'time', 'std']
 
+    # retrieve approx mapl-cirup
+    df_compilation_approx = df.loc[df['solver'] == 'mapl-cirup-approx']
+    df_compilation_approx = df_compilation_approx[['run', 'solver', 'var_num', 'tot_time']]
+    df_compilation_approx = df_compilation_approx.replace('mapl-cirup-approx', 'mapl-cirup-approx (kc+vi)')
+    df_compilation_approx = df_compilation_approx.groupby(['solver', 'var_num'], as_index=False).agg(
+        {'tot_time': ['mean', 'std']})
+    df_compilation_approx.columns = ['solver', 'var_num', 'time', 'std']
+
     df = df[['run', 'solver', 'var_num', 'vi_time']]
-    df = df.replace('mapl-cirup', 'maple-cirup (vi)')
+    df = df.replace('mapl-cirup', 'mapl-cirup (vi)')
     df = df.groupby(['solver', 'var_num'], as_index=False).agg({'vi_time': ['mean', 'std']})
     df.columns = ['solver', 'var_num', 'time', 'std']
 
-    df = pd.concat([df, df_compilation])
+    df = pd.concat([df, df_compilation, df_compilation_approx])
     return df
 
 
@@ -54,16 +62,24 @@ def _get_synth_cross_stitch_data():
 
     df_compilation = df.loc[df['solver'] == 'mapl-cirup']
     df_compilation = df_compilation[['run', 'solver', 'var_num', 'tot_time']]
-    df_compilation = df_compilation.replace('mapl-cirup', 'maple-cirup (kc+vi)')
+    df_compilation = df_compilation.replace('mapl-cirup', 'mapl-cirup (kc+vi)')
     df_compilation = df_compilation.groupby(['solver', 'var_num'], as_index=False).agg({'tot_time': ['mean', 'std']})
     df_compilation.columns = ['solver', 'var_num', 'time', 'std']
 
+    # retrieve approx mapl-cirup
+    df_compilation_approx = df.loc[df['solver'] == 'mapl-cirup-approx']
+    df_compilation_approx = df_compilation_approx[['run', 'solver', 'var_num', 'tot_time']]
+    df_compilation_approx = df_compilation_approx.replace('mapl-cirup-approx', 'mapl-cirup-approx (kc+vi)')
+    df_compilation_approx = df_compilation_approx.groupby(['solver', 'var_num'], as_index=False).agg(
+        {'tot_time': ['mean', 'std']})
+    df_compilation_approx.columns = ['solver', 'var_num', 'time', 'std']
+
     df = df[['run', 'solver', 'var_num', 'vi_time']]
-    df = df.replace('mapl-cirup', 'maple-cirup (vi)')
+    df = df.replace('mapl-cirup', 'mapl-cirup (vi)')
     df = df.groupby(['solver', 'var_num'], as_index=False).agg({'vi_time': ['mean', 'std']})
     df.columns = ['solver', 'var_num', 'time', 'std']
 
-    df = pd.concat([df, df_compilation])
+    df = pd.concat([df, df_compilation, df_compilation_approx])
     return df
 
 
@@ -71,9 +87,13 @@ def create_plot(out_filename=None):
     color_spudd = "tab:red"
     color_mapl_vi = "tab:blue"
     color_mapl_kcvi = "tab:green"
+    color_mapl_approx_vi = "tab:purple"
+    color_mapl_approx_kcvi = "tab:pink"
     label_spudd = "SPUDD"
     label_mapl_vi = "\\texttt{mapl-cirup} (VI)"
     label_mapl_kcvi = "\\texttt{mapl-cirup} (KC+VI)"
+    label_mapl_approx_vi = "\\texttt{mapl-cirup}$_\\approx$ (VI)"
+    label_mapl_approx_kcvi = "\\texttt{mapl-cirup}$_\\approx$ (KC+VI)"
 
     #
     # get data
@@ -100,17 +120,29 @@ def create_plot(out_filename=None):
     assert len(x) > 0
     axs[0].plot(x, y, color=color_spudd, marker=".", label=label_spudd)
 
-    data_mapl_vi = data_cross_stitch[data_cross_stitch["solver"] == "maple-cirup (vi)"]
+    data_mapl_vi = data_cross_stitch[data_cross_stitch["solver"] == "mapl-cirup (vi)"]
     x = data_mapl_vi.loc[data_mapl_vi['var_num'] <= 8, 'var_num']
     y = data_mapl_vi.loc[data_mapl_vi['var_num'] <= 8, 'time']
     assert len(x) > 0
     axs[0].plot(x, y, color=color_mapl_vi, marker=".", label=label_mapl_vi)
 
-    data_mapl_kcvi = data_cross_stitch[data_cross_stitch["solver"] == "maple-cirup (kc+vi)"]
+    data_mapl_kcvi = data_cross_stitch[data_cross_stitch["solver"] == "mapl-cirup (kc+vi)"]
     x = data_mapl_kcvi["var_num"]
     y = data_mapl_kcvi["time"]
     assert len(x) > 0
     axs[0].plot(x, y, color=color_mapl_kcvi, marker=".", label=label_mapl_kcvi)
+
+    data_mapl_vi_approx = data_cross_stitch[data_cross_stitch["solver"] == "mapl-cirup-approx"]
+    x = data_mapl_vi_approx['var_num']
+    y = data_mapl_vi_approx['time']
+    assert len(x) > 0
+    axs[0].plot(x, y, color=color_mapl_approx_vi, marker=".", label=label_mapl_approx_vi)
+
+    data_mapl_approx_kcvi = data_cross_stitch[data_cross_stitch["solver"] == "mapl-cirup-approx (kc+vi)"]
+    x = data_mapl_approx_kcvi["var_num"]
+    y = data_mapl_approx_kcvi["time"]
+    assert len(x) > 0
+    axs[0].plot(x, y, color=color_mapl_approx_kcvi, marker=".", label=label_mapl_approx_kcvi)
 
     axs[0].axhline(y=TIMEOUT, color="gray", linestyle="dashed")
     #axs[0].text(2, TIMEOUT+100, "timeout (600s)", rotation=0)
@@ -130,17 +162,29 @@ def create_plot(out_filename=None):
     assert len(x) > 0
     axs[1].plot(x, y, color=color_spudd, marker=".", label=label_spudd)
 
-    data_mapl_vi = data_chain[data_chain["solver"] == "maple-cirup (vi)"]
+    data_mapl_vi = data_chain[data_chain["solver"] == "mapl-cirup (vi)"]
     x = data_mapl_vi.loc[data_mapl_vi['var_num'] <= 6, 'var_num']
     y = data_mapl_vi.loc[data_mapl_vi['var_num'] <= 6, 'time']
     assert len(x) > 0
     axs[1].plot(x, y, color=color_mapl_vi, marker=".", label=label_mapl_vi)
 
-    data_mapl_kcvi = data_chain[data_chain["solver"] == "maple-cirup (kc+vi)"]
+    data_mapl_kcvi = data_chain[data_chain["solver"] == "mapl-cirup (kc+vi)"]
     x = data_mapl_kcvi["var_num"]
     y = data_mapl_kcvi["time"]
     assert len(x) > 0
     axs[1].plot(x, y, color=color_mapl_kcvi, marker=".", label=label_mapl_kcvi)
+
+    data_mapl_vi_approx = data_chain[data_chain["solver"] == "mapl-cirup-approx"]
+    x = data_mapl_vi_approx['var_num']
+    y = data_mapl_vi_approx['time']
+    assert len(x) > 0
+    axs[1].plot(x, y, color=color_mapl_approx_vi, marker=".", label=label_mapl_approx_vi)
+
+    data_mapl_approx_kcvi = data_chain[data_chain["solver"] == "mapl-cirup-approx (kc+vi)"]
+    x = data_mapl_approx_kcvi["var_num"]
+    y = data_mapl_approx_kcvi["time"]
+    assert len(x) > 0
+    axs[1].plot(x, y, color=color_mapl_approx_kcvi, marker=".", label=label_mapl_approx_kcvi)
 
     axs[1].axhline(y=TIMEOUT, color="gray", linestyle="dashed")
     #axs[0].text(2, TIMEOUT+100, "timeout (600s)", rotation=0)
